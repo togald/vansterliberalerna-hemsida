15262c2133e9c3cd3bf8c6425a598b5838e48d12
MFserver <togald.nilsson@gmail.com>
2013-02-23 01:25:54 +0100

    Lade till kommentarer i source/creator.php

--- source/creator.php
+++ source/creator.php
@@ -6,14 +6,14 @@
 	
 	Variables:
 		$Source - global root directory on the local machine
+		$Content - path to the content template directory
 		$URL - global root directory on the remote machine
-		$globalGeneratedRoot - directory on the local machine to output the generated files to
-	
-		$page - path to the local page template, relative to the global root
-		$content - contents of the HTML page that is being parsed
-		$output - the parse output written to the HTML file
+		$Target - directory on the local machine to output the generated files to
 		$Dirs - array containing all directories that will be used to store HTML files, relative to the actual root directory
+		$i - used to store the current working index of the $Dirs array. There are better ways of doing this. 
 		
+		IMPORTANT!
+		The $URL variable is defined twice. Once for the local path, and once for the remote path, i.e. the URL to the webpage. Be sure to comment out the one you're not using at the moment! 
 	*/
 	
 	$Source = '/home/mfserver/ramdisk/source/';
@@ -23,6 +23,15 @@
 	$Target = '/home/mfserver/ramdisk/target/';
 	$Dirs;
 	$i;
+	
+	/*
+	makePage() is used to parse the contents of a template file, include it in a skeleton and output the resulting code to a raw HTML file in the $Target directory. 
+	
+	$page - path to the local page template, relative to the global root
+	$content - contents of the HTML page that is being parsed
+	$output - the parse output written to the HTML file
+	$file - temporary file handler for an open file
+	*/
 	
 	function makePage($page) {
 		// Import global variables
@@ -57,23 +66,40 @@
 		echo "$status\n";
 	}
 	
+	/*
+	listDirectoryRecursive() is used to recursively list through a directory, and save all the sub-directories into the global $Dirs variable. This function is designed to exclude any normal files, only directories are outputted. 
+	
+	$path - the directory used as a starting point for the recursive scan
+	$level - a counter to keep track of how deep we have scanned
+	$ignore - a list of subdirectories to be ignored when scanning the directory tree
+	$dh - temporary reference to the open directory handler
+	$file - temporary iteration variable that references the current file in the $path
+	*/
+	
 	function listDirectoryRecursive( $path = '.', $level = 0 ) {
-	// Lägga till fktion för att kolla om $path existerar
+		// Import needed global variables
 		global $Dirs, $i;
+		
+		// List of directories to ignore
 		$ignore = array( '.', '..' );
+		
+		// Loop through subdirectories of the $path, exclude normal files
 		$dh = @opendir( $path );
 		while ( false !== ( $file = readdir( $dh ) ) ) {
-			if ( !in_array( $file, $ignore ) ) {
-				if ( is_dir( "$path/$file" ) ) {
-					$Dirs[$i] = "$path/$file";
-					$i++;
-					listDirectoryRecursive ( "$path/$file", $level + 1 );
-				}
+			if ( !in_array( $file, $ignore ) && is_dir( "$path/$file" ) ) {
+				$Dirs[$i] = "$path/$file";
+				$i++;
+				
+				// Call the function again to loop through the subdirectories of the subdirectory...
+				listDirectoryRecursive ( "$path/$file", $level + 1 );
 			}
 		}
 	}
 	
-	function recurse_copy($src,$dst) { 
+	/*
+	recurse_copy() is copied directly from the PHP reference guide examples. The function is used to recursively copy a directory and all of it's content to a new location. 
+	*/
+	function recurse_copy($src,$dst) {
 		$dir = opendir($src); 
 		@mkdir($dst); 
 		while(false !== ( $file = readdir($dir)) ) { 
@@ -88,6 +114,10 @@
 		} 
 		closedir($dir); 
 	} 
+	
+	/*
+	This code will be dealt with later. I'm tired. 
+	*/
 	
 	$directories = scandir($Content);
 	$ignore = array('.', '..');
