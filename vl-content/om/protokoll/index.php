def0c137122d77c9740d78077ab4d25586a0eff7
MFserver <togald.nilsson@gmail.com>
2013-03-06 22:19:29 +0100

    Brutna länkar i sidan som visar styrelseprotokoll fixat

---
+++ vl-content/om/protokoll/index.php
@@ -0,0 +1,29 @@
+<?php
+	$title = "Mötesprotokoll";
+?>
+
+		<h1><?php echo $title ?></h1>
+        <p>Här kan du ladda ner Vänsterliberalernas mötesprotokoll från styrelsemöten och partimöten. Protokollen sammanställs av sekreteraren efter varje mötestillfälle. 
+        
+        <h2>Styrelsemöten</h2>
+        <ul><?php
+            $Dires = array();
+            $dh = @opendir( $Content.'om/protokoll/styrelse' );
+                while ( false !== ( $file = readdir( $dh ) ) ) {
+                    if ( !is_dir($Content.'om/protokoll/styrelse/'.$file) && $file != "index.php" ) {
+                        $Dires[count($Dires)] = $Target.'om/protokoll/styrelse/'.$file;
+                    }
+                }
+                
+                // OBS
+                // natcasesort måste vändas om, nyaste skall vara först!
+                natcasesort($Dires);
+                foreach ( $Dires as $dir ) {
+                    echo '            <li><a href="'.str_replace( $Target, $URL, $dir ).'">'.substr(str_replace( $Target.'om/protokoll/styrelse/', '', $dir ), 0, -4 ).'</a>';
+                }
+        ?></ul>
+        
+        <h2>Partimöten</h2>
+        <ul>
+        
+        </ul>
