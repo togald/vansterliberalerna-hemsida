0c1ed82c73d3d08f03bd518ee8eba4346b493af0
MFserver <togald.nilsson@gmail.com>
2013-03-20 13:23:27 +0100

    Uppdaterad startsida

--- vl-content/index.php
+++ vl-content/index.php
@@ -3,4 +3,10 @@
 ?>
 
 		<h1><?php echo $title ?></h1>
-        <p>
+        <p>Välkommen till det politiska partiet Vänsterliberalernas hemsida! Vänsterliberalerna är ett politiskt parti verksamt i Sverige, som vill kombinera den liberala ideologin med vänsterpolitikens sociala system och omtanke om samhällets svagaste. Vi är övertygade om att det är möjligt att ha ett samhälle där individen har stor individuell frihet, utan att vi för den sakens skull måste underlåta att ge de människor som inte har så gott om pengar rätt till sjukvård, utbildning, och ett drägligt liv. 
+        
+        <p class="strong">Kort sagt, ett humanistiskt parti som värnar individens frihet att bestämma över sitt liv!
+        
+        <p>Om du vill veta mer om partiets politik, kan det vara en bra idé att börja med att titta på <a href="<?php echo $Target; ?>vardegrund/index.html">vår värdegrund</a>. Där står partiets politik sammanfattad, med någr få exempel på verkliga, aktuella frågor. Det är det bästa sättet att snabbt bilda sig en uppfattning om Vänsterliberalernas politik. Om det är en speciell politisk fråga som ligger dig varmt om hjärtat, kan du istället kika på de <a href="<?php echo $Target; ?>politik/index.html">politiska frågorna</a>, för att se vad vi sagt i just den frågan. 
+        
+        <p>Annars kan du kasta dig direkt in i debatten, och debattera med någon partimedlem eller styrelsemedlem på facebok, googe+, på stan, i tidningarna, i bloggosfären, via irc, i demonstrationståg eller något annat forum! Hela partiets styrelse finns tillgänglig för att debattera med vem som helst om vad som helst, även om några av oss är lite mer specialicerade på vissa frågor än de andra. 
