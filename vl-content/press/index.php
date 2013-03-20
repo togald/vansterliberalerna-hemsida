850278fe1fc2d59c3f90e999b43d04fb33d5dd25
MFserver <togald.nilsson@gmail.com>
2013-03-20 17:04:43 +0100

    Uppdaterad press/

--- vl-content/press/index.php
+++ vl-content/press/index.php
@@ -2,5 +2,5 @@
 	$title = "Press";
 ?>
 
-		<h1><?php echo $title ?></h1>
-        <p>
+<h1><?php echo $title ?></h1>
+<p>Här finner du pressmeddelanden som partiet gör, samt grafik som det är fritt fram att ta av och använda i såväl tryckta som digitala publikationer. Licensinformation för fotografier finns i direkt anslutning till varje bild. All Vänsterliberalernas grafik är fri att använda antingen enligt villkoren för WTFPL, Creative Commons eller public domain. 
