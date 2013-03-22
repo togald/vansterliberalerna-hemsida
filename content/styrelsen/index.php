b4b94519435fce8896575d6220e42221a38637e9
MFserver <togald.nilsson@gmail.com>
2013-03-22 09:46:08 +0100

    Uppdaterad manual

--- content/styrelsen/index.php
+++ content/styrelsen/index.php
@@ -5,3 +5,9 @@
 <h1><?php echo $title ?></h1>
 <p>Vänsterliberalerna har en styrelse, som maximalt kan bestå av 15 medlemmar, och som behöver minst tre medlemmar för att partiet ska fungera. Styrelseordföranden är den som i andra partier brukar kallas för partiledare, och väljs på årsbasis, liksom partisekreteraren och partiets kassör. Dock är Vänsterliberalernas politiska program väldigt brett, och att kräva av en person att hen ska kunna representera allt partiet står för är lite opraktiskt. En människa kan helt enkelt inte hålla koll på så många saker samtidigt i debatten. Därför har Vännsterliberalerna istället valt att dela upp det politiska arbetet på hela styrelsen, vilket i praktiken innebär att alla styrelsemedlemmar har den status som en partiledare har i de andra partierna. Styrelsens medlemmar är alla språkrör för partiet. 
 
+<p>För närvarande har styrelsen tre medlemmar, nämligen:
+<ul>
+    <li><a href="<?php echo $URL ?>styrelsen/bard/index.html">Bard Nilsson (ordförande)</a>
+    <li><a href="<?php echo $URL ?>styrelsen/togald/index.html">Togald Nilsson (Sereterare)</a>
+    <li><a href="<?php echo $URL ?>styrelsen/alexander/index.html">Alexander Van Dam (kassör)</a>
+</ul>
