c3a60f12b2eb0956440f89bbb7123db6f275256a
MFserver <togald.nilsson@gmail.com>
2013-03-29 02:37:40 +0100

    Uppdaterad press-sida med anteckningskommentar

--- content/press/index.php
+++ content/press/index.php
@@ -14,3 +14,14 @@
     <img src="<?php echo $URL ?>img/public_domain.png" height=20 width=20 alt="Public Domain">
     <p><a href="Logo.png">PNG</a> <a href="Logo.svg">SVG</a>
 </section>
+
+<!--
+    Vad återstår egentligen?
+    
+    Jo, det ovan ska automatiseras. Dessutom ska scriptet kunna skapa thumbnails för en bildfil automatiskt, samt lägga till länkar till alla varianter som finns av bilden, och tala om hur stora de är, samt pixelstorlek om det är bitmapsbilder. Vi använder:
+     - JPEG
+     - PNG
+     - TIFF
+     - SVG
+    och skriptet måste kunna känna av om det finns en SVG-variant av bilden, baserat på namn. Helst ska skriptet kunna skapa en PNG av en SVG-fil, så att man bara behöver ladda upp TIFF för fotografier, och SVG för grafik. Skriptet körs developer-side, men om en thumbnail redan finns ska det inte genereras nya. 
+-->
