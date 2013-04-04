c0d0cb43cdb13a643cff9bc004eca885c7934c3b
MFserver <togald.nilsson@gmail.com>
2013-04-05 00:57:11 +0200

    uppdaterade styrelse/

--- content/styrelsen/alexander/index.php
+++ content/styrelsen/alexander/index.php
@@ -3,7 +3,16 @@
 ?>
 <?
     $name = "Alexander Van Dam";
+    $birthDate = "";
     $quote = $cite[$name][rand(0,count($cite[$name])-1)];
+    
+    function getAge($then) {
+        $then_ts = strtotime($then);
+        $then_year = date('Y', $then_ts);
+        $age = date('Y') - $then_year;
+        if(strtotime('+' . $age . ' years', $then_ts) > time()) $age--;
+        return $age;
+    }
 ?>
 
 <h1><?php echo $title ?></h1>
@@ -11,4 +20,20 @@
 <blockquote>
     <? echo $quote ?>
 </blockquote>
+<h2>Snabba frågor</h2>
+<dl>
+    <dt>Namn?
+    <dd>Alexander Van Dam
+    <dt>Ålder?
+    <dd><? echo getAge($birthDate) ?> år
+    <dt>Sysselsättning?
+    <dd>
+    <dt>Primära frågor i partiet?
+    <dd>
+    <dt>Intressen?
+    <dd>
+</dl>
 <p>
+<blockquote>
+    
+</blockquote>
--- content/styrelsen/togald/index.php
+++ content/styrelsen/togald/index.php
@@ -3,7 +3,16 @@
 ?>
 <?
     $name = "Togald Nilsson";
+    $birthDate = "1992-05-12";
     $quote = $cite[$name][rand(0,count($cite[$name])-1)];
+    
+    function getAge($then) {
+        $then_ts = strtotime($then);
+        $then_year = date('Y', $then_ts);
+        $age = date('Y') - $then_year;
+        if(strtotime('+' . $age . ' years', $then_ts) > time()) $age--;
+        return $age;
+    }
 ?>
 
 <h1><?php echo $title ?></h1>
@@ -11,4 +20,20 @@
 <blockquote>
     <? echo $quote ?>
 </blockquote>
+<h2>Snabba frågor</h2>
+<dl>
+    <dt>Namn?
+    <dd>Togald Nilsson
+    <dt>Ålder?
+    <dd><? echo getAge($birthDate) ?> år
+    <dt>Sysselsättning?
+    <dd>Studerar till Civilingenjör i energisystem vid Uppsala universitet
+    <dt>Primära frågor i partiet?
+    <dd>Miljöpolitik och immaterialrätt. 
+    <dt>Intressen?
+    <dd>Datorer, teknik i största allmänhet, eld, saker som brinner, musik. 
+</dl>
 <p>
+<blockquote>
+    
+</blockquote>
